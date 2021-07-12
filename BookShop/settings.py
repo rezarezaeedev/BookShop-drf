ddfef1f40aafa124e5da195e6dfdbaecc16c217e
Reza Rezaee <43160556+rezarezaeedev@users.noreply.github.com>
2021-07-12 21:28:46 +0430

    set False for DEBUG mode in setting.py

--- BookShop/settings.py
+++ BookShop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$6rgzdx9#qfx*)m8e(mm0c$)3k*s&+4s5e=!=7!mo!^$fzx)21'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

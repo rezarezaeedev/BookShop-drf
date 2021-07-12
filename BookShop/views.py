5de558416bad24d23b7d55ecab040d39c7c02777
Reza Rezaee <43160556+rezarezaeedev@users.noreply.github.com>
2021-07-12 23:08:26 +0430

    fix detail-data

--- BookShop/views.py
+++ BookShop/views.py
@@ -13,15 +13,15 @@ def home(request):
      <a href='api/v1/get-fav-data/'>Get all favorite Books (GET)</a><br/><br/>
      <a href='api/v1/create-data/'>Create a Book with json data taken (POST)</a><br/><br/>
      <a href='api/v1/search/?name=پایتون&author=پایتون'>Search books for name or author contains `پایتون` </a><br/><br/>
-     <a href='api/v1/detail-data/<1>/'>Show Book detail with `1` id (GET)</a><br/><br/>
-     <a href='api/v1/detail-data/<1>/'>Delete a Book with `1` id (DELETE)</a><br/><br/>
-     <a href='api/v1/detail-data/<1>/'>Update a Book with `1` id (PUT)</a><br/><br/>
+     <a href='api/v1/detail-data/1/'>Show Book detail with `1` id (GET)</a><br/><br/>
+     <a href='api/v1/detail-data/1/'>Delete a Book with `1` id (DELETE)</a><br/><br/>
+     <a href='api/v1/detail-data/1/'>Update a Book with `1` id (PUT)</a><br/><br/>
      <a href='api/v1/get-all-data/'>Get all Books(GET)</a><br/><br/>
      <br><hr><br>
      
    </center>    
      <h1>Correct json data</h1>
-   <pre style='text-align: left;'>
+   <pre style='text-align: left;font-size=20;'>
         {
             "name": "ترفند های پایتون",
             "author": "محمد بابازاده",

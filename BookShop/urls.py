4e440f921987adb2088c4428b6f6be96b38aeeb8
Reza Rezaee <43160556+rezarezaeedev@users.noreply.github.com>
2021-07-12 21:32:16 +0430

    add home path

--- BookShop/urls.py
+++ BookShop/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 from BookShop import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/',include('tp_book.urls'))
+    path('api/v1/',include('tp_book.urls')),
+    path('',home,name='home')
 ]
 
 
